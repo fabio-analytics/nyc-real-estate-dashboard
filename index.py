2b0c605c6582df3cb227790bc57ed5a7268add5b
Fabio santana de castro <fabiobook23@gmail.com>
2026-03-08 21:06:35 -0300

    corrige app.run

--- index.py
+++ index.py
@@ -114,4 +114,4 @@ def update_hist(location, square_size, color_map):
     return hist_fig, map_fig
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=8050, debug=False)
